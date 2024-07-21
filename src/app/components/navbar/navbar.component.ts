be9dfdac7b651e18852c17871034acdbbf0a40ea
milltoy <pornratmill@gmail.com>
2024-07-20 21:38:12 -0400

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+    private readonly router = inject(Router);
 
+    goToApprovalPage() {
+        this.router.navigateByUrl('/app-approval-page');
+    }
 }
