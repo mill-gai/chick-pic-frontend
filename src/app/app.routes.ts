1f2422ca2169ccf0208d9ebf241d22b1807959b7
milltoy <pornratmill@gmail.com>
2024-07-20 21:38:38 -0400

    approval page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ApprovalPageComponent } from './pages/approval-page/approval-page.component';
 
 export const routes: Routes = [
-    {path: '', component: HomePageComponent},
+    { path: '', component: HomePageComponent },
+    { path: 'app-approval-page', component: ApprovalPageComponent },
 ];
---
+++ src/app/pages/approval-page/approval-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './approval-page.component.html',
+  styleUrl: './approval-page.component.css'
+})
+export class ApprovalPageComponent {
+
+}
