cd6985cf0aec05dee22f8c7c2cfec5ed6d0c4d13
milltoy <pornratmill@gmail.com>
2024-07-22 18:12:30 -0400

    Update location.service.ts

--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Location } from '../../model/location';
+import { Items } from '../../model/dropdown-item';
 
 @Injectable({
     providedIn: 'root',
@@ -9,9 +10,16 @@ import { Location } from '../../model/location';
 export class LocationService {
     constructor(private httpClient: HttpClient) {}
 
-    getAllLocations(): Observable<Location[]> {
-        return this.httpClient.get<Array<Location>>(
-            'http://localhost:9000/api/location'
+    getAllCountries(): Observable<Items[]> {
+        return this.httpClient.get<Items[]>(
+            'http://localhost:8081/api/location/getAllCountries'
+        );
+    }
+
+    getCitiesByCountry(country: string): Observable<Location> {
+        return this.httpClient.get<Location>(
+            'http://localhost:9000/api/location/getCitiesByCountry',
+            { params: { country: country } }
         );
     }
 }
