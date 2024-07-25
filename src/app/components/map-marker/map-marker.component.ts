8f74adf824d7737630e6afbfe56a482a725dd659
milltoy <pornratmill@gmail.com>
2024-07-24 22:53:34 -0400

    add load more

--- src/app/components/map-marker/map-marker.component.ts
+++ src/app/components/map-marker/map-marker.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject, signal } from '@angular/core';
 import { MapAdvancedMarker } from '@angular/google-maps';
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Image } from '../../model/image';
+import { Image, ImageInfo } from '../../model/image';
 import { Position } from '../../model/position';
 import { ImageService } from '../../services/image/image.service';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-map-marker',
@@ -18,40 +19,72 @@ export class MapMarkerComponent implements OnInit {
     @Input() country: string;
     @Input() city: string;
     @Input() position: Position;
-    @Input() images: Image[];
-    imgUrls: string[];
+    images: ImageInfo[] = [];
+    imgUrls: string[] = [];
     showContent: boolean = false;
     faCircleXmark = faCircleXmark;
+    page = signal(0);
+    page$ = toObservable(this.page);
+    pageSize = 2;
+    isLastPage = false;
 
     ngOnInit(): void {
-        // if (this.country in coord && this.city in coord[this.country]) {
-        //     this.position = coord[this.country][this.city];
-        // } else {
-        //     this.position = { lat: 50, lng: -72 };
-        // }
-        const fileNames: string[] = this.images.map((i) => i.fileName);
-        this.getImageUrl(fileNames);
+        // const fileNames: string[] = this.images.map((i) => i.fileName);
+        this.page$.subscribe((curPage) => {
+            console.log('update page');
+            this.imageService
+                .getImageByPage(this.city, this.page(), this.pageSize)
+                .pipe()
+                .subscribe((response) => {
+                    const prevLen = this.images.length;
+                    this.images = this.images.concat(response.images);
+                    this.isLastPage = response.isLast;
+                    const fileNames: string[] = response.images.map(
+                        (i) => i.fileName
+                    );
+                    this.getImageUrl(fileNames, prevLen);
+                    console.log(this.images);
+                });
+        });
+        // this.page$.subscribe((curPage) => {
+        //     console.log('page update');
+        //     this.imageService
+        //         .getImageByPage(this.city, curPage, this.pageSize)
+        //         .pipe()
+        //         .subscribe((response) => {
+        //             this.images.concat(response.images);
+        //             this.isLastPage = response.isLast;
+        //             this.getImageUrl(fileNames);
+        //         });
+        // });
     }
 
-    getImageUrl(fileNames: string[]) {
+    getImageUrl(fileNames: string[], start: number) {
         this.imageService
             .getImageUrls(fileNames)
             .pipe()
             .subscribe((response) => {
-                this.images.forEach(
-                    (value, index) => (value.fileUrl = response[index])
+                response.forEach(
+                    (value, index) =>
+                        (this.images[start + index].fileUrl = value)
                 );
+                // this.images.forEach(
+                //     (value, index) => (value.fileUrl = response[index])
+                // );
             });
-        console.log('imggg: ' + this.images);
     }
 
     onClickHandler() {
         this.showContent = !this.showContent;
+        // this.page.set(this.page() + 1);
+    }
+
+    onClickViewMoreHandler() {
+        this.page.set(this.page() + 1);
     }
 
     onClickContainer(event: Event) {
         const popup = document.getElementById('outsidePopup');
-        console.log(event.target);
         if (event.target == popup) {
             this.showContent = !this.showContent;
         }
